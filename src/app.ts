24992d047a056e602fd39cf3f94af1424fd23425
Masum-Osman <ogkmasum@gmail.com>
2025-05-08 21:02:12 +0600

    feat: add healthcheck route and controller

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
+import healthRoutes from './api/routes/health'
+
 
 const app = express()
 app.use(express.json())
-
+app.use('/api', healthRoutes)
 export default app
